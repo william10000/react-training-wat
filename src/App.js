a9bbb2a8b7c05e6c8faae12f7d20c249ffbeedc6
William Wan <wwan@athenahealth.com>
2019-11-06 14:05:27 -0500

    added input and label with htmlFor attribute

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 function App() {
+  const h1Style = {
+    color: "red",
+    fontSize: "32px"
+  };
+
   return (
     <>
-      <h1>Hi from function</h1>
-      <p>paragraph</p>
+      <h1 className="header" style={h1Style}>
+        Hi from function
+      </h1>
+      <label htmlFor="firstName">First Name</label>
+      <input id="firstName" type="text"></input>
+      <p>Paragraph</p>
     </>
   );
 }
