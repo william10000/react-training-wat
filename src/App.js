44f7b10a67ed9ce352d7489c9728b44e3e504bf3
William Wan <wwan@athenahealth.com>
2019-11-07 11:14:11 -0500

    added bootstrap, extracted User component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { getUsers, deleteUser } from "./api/userApi";
-import "./app.css";
+import React from "react";
+import Users from "./Users";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  // useEffect runs by default after every render
-  useEffect(() => {
-    getUsers().then(_users => setUsers(_users));
-  }, []); // array contains data where if the data changes, we want to run useEffect
-
-  // updates to state and db are not in a transaction so they could be out of sync if one fails
-  function handleDelete(id) {
-    const newUsers = users.filter(user => user.id !== id);
-    deleteUser(id).then(() => {
-      setUsers(newUsers);
-    });
-  }
-
-  return (
-    <>
-      <h1 className="header">Users</h1>
-      {/* display user data in a table with headers for id, name, email */}
-      <table className="table">
-        <thead>
-          <tr>
-            <th></th>
-            <th>id</th>
-            <th>name</th>
-            <th>email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>
-                <button onClick={() => handleDelete(user.id)}>Delete</button>
-              </td>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <label htmlFor="firstName">First Name</label>
-      <input id="firstName" type="text"></input>
-      <p>Paragraph</p>
-    </>
-  );
+  return <Users />;
 }
 
 export default App;
---
+++ src/Users.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { getUsers, deleteUser } from "./api/userApi";
+import "./app.css";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then(_users => setUsers(_users));
+  }, []); // array contains data where if the data changes, we want to run useEffect
+
+  function handleDelete(id) {
+    const newUsers = users.filter(user => user.id !== id);
+    deleteUser(id).then(() => {
+      setUsers(newUsers);
+    });
+  }
+
+  return (
+    <>
+      <h1 className="header">Users</h1>
+      {/* display user data in a table with headers for id, name, email */}
+      <table className="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>
+                <button onClick={() => handleDelete(user.id)}>Delete</button>
+              </td>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <label htmlFor="firstName">First Name</label>
+      <input id="firstName" type="text"></input>
+      <p>Paragraph</p>
+    </>
+  );
+}
+
+export default Users;
