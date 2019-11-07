4a7739be4aca89aa5110e2dd278c87017a12efb2
William Wan <wwan@athenahealth.com>
2019-11-07 10:45:25 -0500

    converted user list to table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getUsers, deleteUser } from "./api/userApi";
+import "./app.css";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -9,32 +10,40 @@ function App() {
     getUsers().then(_users => setUsers(_users));
   }, []); // array contains data where if the data changes, we want to run useEffect
 
-  const h1Style = {
-    color: "blue",
-    fontSize: "32px"
-  };
-
   // updates to state and db are not in a transaction so they could be out of sync if one fails
   function handleDelete(id) {
     const newUsers = users.filter(user => user.id !== id);
-    deleteUser(id);
-    setUsers(newUsers);
+    deleteUser(id).then(() => {
+      setUsers(newUsers);
+    });
   }
 
   return (
     <>
-      <h1 className="header" style={h1Style}>
-        Users
-      </h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            {/* Use arrow function on onClick so you don't invoke on render */}
-            <button onClick={() => handleDelete(user.id)}>Delete</button>
-            name: {user.name} - email: {user.email}
-          </li>
-        ))}
-      </ul>
+      <h1 className="header">Users</h1>
+      {/* display user data in a table with headers for id, name, email */}
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>
+                <button onClick={() => handleDelete(user.id)}>Delete</button>
+              </td>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <label htmlFor="firstName">First Name</label>
       <input id="firstName" type="text"></input>
       <p>Paragraph</p>
